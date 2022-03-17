7560177209c28ac83c9744d0a0afe5ecd8fdaa44
NBS5000 <steve.nbs.barry@gmail.com>
2022-03-18 00:49:42 +1100

    deleted unused css file, styling added and hb adjusted for items in profile, image files added for star wars credits

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,17 +1,20 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
 
-// Get the button that opens the modal
-var btn = document.getElementById("profileList");
 
 // Get the <span> element that closes the modal
 var canx = document.getElementById("canx");
-
+var canx2 = document.getElementById("canx2");
 // When the user clicks the "Begin"  button, open the modal 
 
 var modalBtn = document.getElementById("profileList");
 modalBtn.addEventListener("click",function(){
     modal.style.display = "block";
+});
+var modalBtn2 = document.getElementById("profileList2");
+modalBtn2.addEventListener("click",function(){
+    modal2.style.display = "block";
 });
 
 // When the user clicks the "Cancel" button, close the modal and clear fields
@@ -25,13 +28,23 @@ canxBtn.addEventListener("click",function(){
     modal.style.display = "none";
 });
 
+var canxBtn2 = document.getElementById("canx");
+canxBtn2.addEventListener("click",function(){
+    modal2.style.display = "none";
+});
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {    
         document.getElementById("name").value = "";
-    document.getElementById("price").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("image_link").value = "";
+        document.getElementById("price").value = "";
+        document.getElementById("description").value = "";
+        document.getElementById("image_link").value = "";
         modal.style.display = "none";
+    }
+}
+window.onclick = function(event) {
+    if (event.target == modal2) {    
+        modal2.style.display = "none";
     }
 }
